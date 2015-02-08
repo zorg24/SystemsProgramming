ae3c2e5f81432bf485f843aebfac93e8d3aa0e74
Nathan <nathan@saslavsky.com>
2015-02-08 16:28:51 -0700

    Fixed constructor and added constants for directions

--- HW2/HW2/SlidingPuzzleState.cpp
+++ HW2/HW2/SlidingPuzzleState.cpp
@@ -1,11 +1,27 @@
 #include "SlidingPuzzleState.h"
+#include <iostream>
 
 
 SlidingPuzzleState::SlidingPuzzleState(const int tiles_[12])
 {
 	for (int i = 0; i < 12; ++i)
 	{
-		tiles[i] = tiles_[i];
+		for (int j = i; tiles_[i] != tiles_[j]; ++j)
+		{
+			std::cerr << "The input contains a duplicate entry" << std::endl;
+		}
+	}
+
+	for (int i = 0; i < 12; ++i)
+	{
+		if (tiles_[i] >= 0 && tiles_[i] < 12)
+		{
+			tiles[i] = tiles_[i];
+		}
+		else
+		{
+			std::cerr << "The " << i << "th element of the input contains an element outside of the range [0,12).";
+		}
 	}
 }
 
--- HW2/HW2/SlidingPuzzleState.h
+++ HW2/HW2/SlidingPuzzleState.h
@@ -13,6 +13,7 @@ public:
 	void Print();
 	SlidingPuzzleState *Clone();
 	bool IsSolution();
+	const int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
 private:
 	int tiles[12];
 };
