38a9988bba21f9e0506b0dd485c45b7bd6aeebe4
Keiwaiou <kristin.douglass@gmail.com>
2015-03-04 14:07:57 -0700

    added func to .cpp file

--- HW4/HW4/HW4/PagedDiskArray.cpp
+++ HW4/HW4/HW4/PagedDiskArray.cpp
@@ -7,3 +7,61 @@
 #include "PagedDiskArray.h"
 
 // Implement the PagedDiskArray class here
+PagedDiskArray::PagedDiskArray(size_t pageSize, size_t numPages, const char *fileName)
+{
+    
+}
+
+PagedDiskArray::~PagedDiskArray()
+{
+    
+}
+
+uint8_t PagedDiskArray::operator[](size_t index)
+{
+    
+}
+
+
+void PagedDiskArray::set(size_t index, uint8_t value)
+{
+    
+}
+
+
+void PagedDiskArray::WritePageIfDirty(PageFrame *f)
+{
+    
+}
+
+// Write all dirty pages to the disk file
+void PagedDiskArray::Flush()
+{
+    
+}
+
+// Load page into frame
+void PagedDiskArray::LoadPage(size_t pageNum, PageFrame *f)
+{
+    
+}
+// Map page number to page frame in memory - returns nullptr if not present
+PageFrame* PagedDiskArray::GetPageFrame(size_t pageNum)
+{
+    
+}
+
+// Choose best frame to replace in memory (smallest timeLoaded)
+PageFrame* PagedDiskArray::ChooseReplacementFrame()
+{
+    
+}
+// Get pointer to desired array element, writing and reading
+// page frames as necessary.  Set page frame dirty flag if
+// dirty is true;
+uint8_t* PagedDiskArray::GetElement(size_t index, bool dirty)
+{
+    
+}
+
+
